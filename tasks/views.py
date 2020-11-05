f1a0193da818a653fadb4b098f9eac3c46feec9e
rajkishore2001 <rajkishore4kv@gmail.com>
2020-11-05 16:56:02 +0530

    Added Insert and delete todo functionality

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello world')
+def list_todo_items(request):
+    tasks = Task.objects.all()
+    context = {'tasks':tasks}
+    return render(request, 'tasks/todo_list.html', context)
+
+# why HttpResponse ?
+def insert_todo_item(request:HttpResponse):
+    todo = Task(title=request.POST['title'])
+    todo.save()
+    return redirect('/todos/lists/')
+
+def delete_todo_item(request,todo_id):
+    todo_to_delete = Task.objects.get(id=todo_id)
+    todo_to_delete.delete()
+    return redirect('/todos/lists/')
